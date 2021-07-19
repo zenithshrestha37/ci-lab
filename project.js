b3e8b6195ed31313059b8e507a23ed7eb7afca14
zenithshrestha37 <zenithshrestha37@gmail.com>
2021-07-19 14:27:00 -0500

    new test

--- project.js
+++ project.js
@@ -2,4 +2,30 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
+
 module.exports = getGreeting;
+
+//////////////////
+const { expect } = require("@jest/globals")
+let colors = ['Red', 'Green', 'Blue']
+
+module.exports = {
+    stateAge: (age)=> {
+        return ` I am ${age}`
+    },
+    addUser: (name, age) => {
+        let user = {
+            name,
+            age
+        }
+        if (user.age >= 21) {
+            return user
+        } else {
+            return 'User not old enough'
+        }
+    },
+    addColor: (color) => {
+        colors.push(color.toLowerCase())
+        return colors
+    }
+}
--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-const getGreeting = require('./project');
+// const getGreeting = require('./project');
 
-test('can create a basic greeting', () => {
-  expect(getGreeting('Sam')).toBe("Hi Sam");
-});
+// test('can create a basic greeting', () => {
+//   expect(getGreeting('Sam')).toBe("Hi Sam");
+// });
+////////////////////////////////////////
+const { test, expect } = require("@jest/globals");
+const { stateAge, addUser, addColor } = require('./project')
+
+test("Should return I am age {age}", () => {
+    expect(stateAge(37)).toEqual(" I am 37")
+} )
+
+test("Should return the user obj or failed status", () => {
+    expect(addUser('Joey', 21)).toEqual({name: 'Joey', age: 21})
+    expect(addUser('Chandler', 15)).toEqual('User not old enough')
+    
+})
+
+test ("Should add new color and return list of unique colors", () => {
+    expect(addColor('pink')).toContain('pink')
+
+})
